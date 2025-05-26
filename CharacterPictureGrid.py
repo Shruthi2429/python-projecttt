2038c1d9452072bb4b0a539701a5c02077450067
Shruthi2429 <shruthi.24.beai@acharya.ac.in>
2025-05-26 14:04:12 +0530

    Create CharacterPictureGrid.py

---
+++ CharacterPictureGrid.py
@@ -0,0 +1,18 @@
+Shruthi.p/1AY24AI104/osec
+grid = [
+    ['.', '.', '.', '.', '.', '.'],
+    ['.', 'O', 'O', '.', '.', '.'],
+    ['O', 'O', 'O', 'O', '.', '.'],
+    ['O', 'O', 'O', 'O', 'O', '.'],
+    ['.', 'O', 'O', 'O', 'O', 'O'],
+    ['O', 'O', 'O', 'O', 'O', '.'],
+    ['O', 'O', 'O', 'O', '.', '.'],
+    ['.', 'O', 'O', '.', '.', '.'],
+    ['.', '.', '.', '.', '.', '.']
+]
+
+# Print rotated image
+for col in range(len(grid[0])):
+    for row in range(len(grid)):
+        print(grid[row][col], end='')
+    print()
